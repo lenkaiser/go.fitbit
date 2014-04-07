82ddbee3243351e36cafc0dc1d7b0d57b2bec864
leonk <l.keijzer@foize.com>
2014-04-07 10:33:45 +0200

    Added support for water

--- water.go
+++ water.go
@@ -1,23 +1,92 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"strconv"
+	"time"
 )
+
+type WaterUnit struct {
+	Amount uint64 `json:"amount"`
+	LogID  uint64 `json:"logId"`
+}
+
+// Water object contains all the data of the logged water
+type Water struct {
+	Summary *Summary     `json:"summary"`
+	Water   []*WaterUnit `json:"water"`
+}
 
 // GetWater gets the amount of water taken on a specific date
 // It returns an collection of Water or an error if one occours
-func (c *Client) GetWater(date string) (*Water, error) {
-	return nil, errors.New("not implemented yet")
+func (c *Client) GetWater(date time.Time) (*Water, error) {
+	//Build and get request-URL
+	requestURL := fmt.Sprintf("user/-/log/water/date/%s.json", date.Format("2006-01-02"))
+	responseBody, err := c.getData(requestURL)
+	if err != nil {
+		return nil, err
+	}
+
+	//Parse data
+	waterData := &Water{}
+	err = json.NewDecoder(responseBody).Decode(waterData)
+	if err != nil {
+		return nil, err
+	}
+
+	return waterData, nil
 }
 
 // LogWater adds a certain amount of water taken on a specific date
 // It returns an error if one occours
-func (c *Client) LogWater(date, waterUnit string, amount float64) error {
-	return errors.New("not implemented yet")
+func (c *Client) LogWater(measurementType string, amount float64, date time.Time) (*Water, error) {
+	//Supported unit types
+	measurementUnitTypes := map[string]string{"ml": "", "fl oz": "", "cup": ""}
+
+	//Build arguments map
+	dataArguments := map[string]string{
+		"amount": strconv.FormatFloat(amount, 'f', 2, 32),
+		"date":   date.Format("2006-01-02"),
+	}
+
+	//Check parameters
+	if amount > 0 {
+		return nil, errors.New("missing paramters")
+	}
+
+	//Check if correct measurement is passed
+	_, ok := measurementUnitTypes[measurementType]
+	if ok {
+		dataArguments["unit"] = measurementType
+	}
+
+	//Build an put request-URL
+	responseBody, err := c.postData("user/-/foods/log/water.json", dataArguments)
+	if err != nil {
+		return nil, err
+	}
+
+	//Parse data
+	logWater := &Water{}
+	err = json.NewDecoder(responseBody).Decode(logWater)
+	if err != nil {
+		return nil, err
+	}
+
+	return logWater, nil
 }
 
 // DeleteWater removes a record of water taken based on the waterId
 // It returns an error if one occours
 func (c *Client) DeleteWater(waterId uint64) error {
-	return errors.New("not implemented yet")
+	//Build requestURL and DELETE data
+	requestURL := fmt.Sprintf("usr/-/foods/log/water/%i.json", waterId)
+	_, err := c.deleteData(requestURL, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
